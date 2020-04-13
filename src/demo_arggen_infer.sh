5ae37f632fb08f742c7cb12e5d850ac2ff941553
Jeff <jeffrey110616@gmail.com>
2020-04-13 15:55:18 +0800

    ckpt

--- src/demo_arggen_infer.sh
+++ src/demo_arggen_infer.sh
@@ -1,8 +1,8 @@
 python main.py --mode=predict \
-    --exp_name=arggen_ours_mt \
+    --exp_name=arggen_mt_20 \
     --encode_passage \
     --replace_unk \
     --type_conditional_lm \
     --task=arggen \
-    --batch_size=8 \
-    --load_model_path=epoch_96_train_331.5111_val_227.5992_ppl_2.2354.tar
+    --batch_size=32 \
+    --load_model_path=epoch_311_train_25.7030_val_2.8727_ppl_1.0104.tar
