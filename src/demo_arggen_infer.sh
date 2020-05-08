bc0ce8ece09e1c17c2361509811179bc911753d3
Jeff <jeffrey110616@gmail.com>
2020-05-08 14:45:18 +0800

    pure

--- src/demo_arggen_infer.sh
+++ src/demo_arggen_infer.sh
@@ -1,10 +1,10 @@
 python main.py --mode=predict \
-    --exp_name=arggen_mt_trans \
+    --exp_name=arggen_mt_trans_100k \
     --encode_passage \
     --replace_unk \
     --type_conditional_lm \
     --infer_fold=4 \
-    --infer_fold_selected=3 \
+    --infer_fold_selected=4 \
     --task=arggen \
     --batch_size=48 \
-    --load_model_path=epoch_49_train_198.8612_val_257.5234_ppl_15.7782.tar
+    --load_model_path=epoch_32_train_170.9898_val_197.8458_ppl_8.1034.tar
